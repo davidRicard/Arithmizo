677d82581c2a3fb9fd9345054575a16a178705bb
hayek <hayek@Frederiques-MBP.fritz.box>
2017-11-12 20:32:15 +0100

    Adding iteration

---
+++ count.py
@@ -0,0 +1,15 @@
+import tweepy
+
+f = open("tw.config","r")
+cKey = f.readline()
+cSecret = f.readline()
+aToken = f.readline()
+aTokenSecrect = f.readline()
+
+auth = tweepy.OAuthHandler(ckey, cSecret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+	print tweet.txt
